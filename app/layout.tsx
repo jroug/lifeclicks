071a470273760a7cde97ca99a7cac419294baa13
jroug <rougeris@gmail.com>
2025-01-07 14:17:19 +0200

    coocie script

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,6 +44,20 @@ export default async function RootLayout({
         <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
         <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
+        {/* Cookie script */}
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function() {
+                  var script = document.createElement('script');
+                  script.type = 'text/javascript';
+                  script.charset = 'UTF-8';
+                  script.src = '//cdn.cookie-script.com/s/635bcf9b4f92cd98f60f2286c1371185.js';
+                  document.head.appendChild(script);
+                })();
+              `,
+            }}
+          />
       </head>
       <body className="antialiased custom-padding-top bg-black">
         {/* Header and Main Menu */}
