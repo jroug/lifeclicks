fe3c6547b0d36f37c4c804f5d0ecc01662c49cd4
jroug <rougeris@gmail.com>
2025-02-11 10:56:55 +0200

    grab cursor remove

--- app/project/[slug]/ProjectPageClient.tsx
+++ app/project/[slug]/ProjectPageClient.tsx
@@ -83,7 +83,6 @@ const ProjectPageClient: React.FC<ProjectPageClientProps> = ({
           mousewheel={{
             sensitivity: 10,
           }}
-          grabCursor={true}
           className="w-full h-full"
           onSlideChange={handleSlideChange}
           breakpoints={{
